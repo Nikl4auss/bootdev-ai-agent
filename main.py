d2a9a4f63c0043e3c39b6af7ef5db20584d589b5
Nicolas Marinelli <nicolasmarinelli@protonmail.com>
2025-07-12 20:32:34 -0300

    Added initial config to accept user messages in a chat-like functionality, and a 'verbose' option when running the script

--- main.py
+++ main.py
@@ -3,20 +3,30 @@ import sys
 from dotenv import load_dotenv
 
 from google import genai
+from google.genai import types 
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
+
 def main():
     if len(sys.argv) < 2:
         print("Error: An user prompt was expected")
         exit(code=1)
+    
     user_prompt = sys.argv[1]
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=user_prompt)
+    
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)])
+    ]
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
 
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
+    if len(sys.argv) > 2:
+        if sys.argv[2] == "--verbose":
+            print(f"User prompt: {user_prompt}\nPrompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
 
+    
 if __name__ == "__main__":
     main()
