38dde6f192250791130cd051a7d3bae4562a0ea5
Nicolas Marinelli <nicolasmarinelli@protonmail.com>
2025-07-12 15:44:32 -0300

    Modified the program to accept user propmt from the terminal

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 
 from google import genai
@@ -8,7 +9,11 @@ api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 def main():
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    if len(sys.argv) < 2:
+        print("Error: An user prompt was expected")
+        exit(code=1)
+    user_prompt = sys.argv[1]
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=user_prompt)
 
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
