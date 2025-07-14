88743165355a1df8592adec0f6ff2d3ae801a053
Nicolas Marinelli <nicolasmarinelli@protonmail.com>
2025-07-14 18:09:29 -0300

    Created the get_files_info function and some basic tests to test it

---
+++ functions/get_files_info.py
@@ -0,0 +1,24 @@
+import os
+
+def get_files_info(working_directory, directory=None):
+  try:
+
+    full_path = os.path.abspath(os.path.join(working_directory, directory))
+
+    if not os.path.abspath(working_directory) in full_path or directory.startswith(".."):
+      return f"Error: Cannot list \"{directory}\" as it is outside the permitted working directory"
+
+    if not os.path.isdir(full_path):
+      return f"Error: \"{directory}\" is not a directory"
+    
+    formated_output = ""
+    for item in os.listdir(full_path):
+      item_path = f"{full_path}/{item}"
+      is_dir = os.path.isdir(item_path)
+      file_size = os.path.getsize(item_path)
+      formated_output += f"{item}: file_size={file_size} bytes, is_dir={is_dir}\n"
+
+    return formated_output
+  except Exception as e:
+    return f"Error: {e}"
+print(get_files_info(".", ""))
